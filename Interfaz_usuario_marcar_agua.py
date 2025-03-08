35037d38752430d4393a3161e19af3fcc90c513a
moraloamg@gmail.com <moraloamg@gmail.com>
2025-03-08 18:28:34 +0100

    Versión final

--- Interfaz_usuario_marcar_agua.py
+++ Interfaz_usuario_marcar_agua.py
@@ -1,8 +1,23 @@
+"""
+Modulo que contiene la interfaz gráfica de usuario.
+"""
+
+import os
+from pathlib import Path
 import tkinter as tk
 from tkinter import filedialog, messagebox, colorchooser, Label
 import fitz  # PyMuPDF para validar PDFs
 from PIL import Image,ImageTk
 from marcar_agua_pdf import anadir_marca_agua_a_pdf, vista_previa_pdf  # Importamos la función desde el otro fichero
+
+__author__ = "Adrian Mateos"
+__copyright__ = "Copyright 2025"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = ""
+__email__ = "https://github.com/moraloamg"
+__status__ = "Completed"
 
 def seleccionar_pdf_entrada():
     """Abre un diálogo para seleccionar el PDF de entrada y valida que sea un archivo PDF válido."""
@@ -13,7 +28,39 @@ def seleccionar_pdf_entrada():
             entrada_var.set(ruta)
         except Exception:
             messagebox.showerror("Error", "El archivo PDF seleccionado está corrupto o no es válido.")
-            
+
+
+def seleccionar_imagen():
+    """Abre un diálogo para seleccionar una imagen valida."""
+    ruta = filedialog.askopenfilename(filetypes=[("Imágenes en jpg, jpeg o png", "*.jpg;*.jpeg;*.png")])
+    if ruta:
+        try:
+            fitz.open(ruta).close()  # Intenta la imagen para ver que no está corrupta
+            entrada_imagen_var.set(ruta)
+            entrada_texto.config(state="disabled")
+            #texto_var.set("")
+            entrada_color.config(state="disabled")
+            #color_var.set("")
+            #color_label.config(bg=root.cget("bg"), text="")
+            marco_color.config(state="disabled")
+            entrada_mayus.config(state="disabled")
+            entrada_minus.config(state="disabled")
+        except Exception:
+            messagebox.showerror("Error", "La imágen está corrupta o no es válida.")
+
+
+def quitar_imagen():
+    ruta = entrada_imagen_var.get()
+    if ruta:
+        try:
+            entrada_imagen_var.set("Aún no se ha elegido una imagen")
+            entrada_texto.config(state="normal")
+            entrada_color.config(state="normal")
+            marco_color.config(state="normal")
+            entrada_mayus.config(state="normal")
+            entrada_minus.config(state="normal")
+        except Exception:
+            messagebox.showerror("Error", "La imágen está corrupta o no es válida.")
 
 
 def lugar_de_guardado():
@@ -32,7 +79,7 @@ def seleccionar_color():
     if color:
         color_var.set(f"({int(color[0])}, {int(color[1])}, {int(color[2])})")
         # Actualiza el label del color con un pequeño recuadro de color y elimina la tupla
-        color_label.config(bg=f'#{int(color[0]):02x}{int(color[1]):02x}{int(color[2]):02x}', text="")
+        marco_color.config(bg=f'#{int(color[0]):02x}{int(color[1]):02x}{int(color[2]):02x}', text="")
 
 def validar_longitud_texto(*args):
     """Limita la entrada de texto a 30 caracteres."""
@@ -42,18 +89,21 @@ def validar_longitud_texto(*args):
 
 def generar_vista_previa():
     """Genera una vista previa de la marca de agua."""
-    if not entrada_var.get() or entrada_var.get() == "Aún no se ha elegido un archivo PDF":
-        messagebox.showerror("Error", "Para usar la vista previa deberás indicar el archivo PDF, el lugar de guardado, el texto de la marca de agua y su color.")
+
+    mensaje_de_error = "Para usar la vista previa deberás indicar el archivo PDF, el nombre del archivo y lugar de guardado, el texto de la marca de agua y su color, o bien una imagen."
+
+    if not os.path.isabs(entrada_var.get()) or not os.path.isabs(salida_var.get()) or not nombre_archivo_var.get():
+        messagebox.showerror("Error", mensaje_de_error)
         return
-    if not salida_var.get() or salida_var.get() == "Aún no se ha elegido un lugar de guardado":
-        messagebox.showerror("Error", "Para usar la vista previa deberás indicar el archivo PDF, el lugar de guardado, el texto de la marca de agua y su color.")
+    if not texto_var.get().strip() and not os.path.isabs(entrada_imagen_var.get()): 
+        messagebox.showerror("Error", mensaje_de_error)
         return
-    if not texto_var.get().strip():
-        messagebox.showerror("Error", "Para usar la vista previa deberás indicar el archivo PDF, el lugar de guardado, el texto de la marca de agua y su color.")
+    if not color_var.get() and not os.path.isabs(entrada_imagen_var.get()):
+        messagebox.showerror("Error", mensaje_de_error)
         return
-    if not color_var.get():
-        messagebox.showerror("Error", "Para usar la vista previa deberás indicar el archivo PDF, el lugar de guardado, el texto de la marca de agua y su color.")
-        return
+    
+    if os.path.isfile(entrada_imagen_var.get()) and not color_var.get():
+        color_var.set((0,0,0))
 
     try:
         # Generar vista previa
@@ -65,6 +115,7 @@ def generar_vista_previa():
             texto_mayusculas=mayusculas_var.get(),
             color_texto=eval(color_var.get()),
             blanco_y_negro=bn_var.get(),
+            ruta_imagen=entrada_imagen_var.get()
         )
 
         if imagen_previa:
@@ -88,14 +139,17 @@ def ejecutar():
     """Valida los datos y ejecuta la función de marca de agua."""
     try:
         # Validaciones
-        if not entrada_var.get() or not salida_var.get():
-            raise ValueError("Debes seleccionar el archivo PDF y la carpeta donde guardarlo.")
-        if not texto_var.get().strip():  # Verifica que el texto no esté vacío
-            raise ValueError("Debes introducir el texto de la marca de agua.")
-        if not color_var.get():
+        if not os.path.isabs(entrada_var.get()) or not os.path.isabs(salida_var.get()) or not nombre_archivo_var.get():
+            raise ValueError("Debes seleccionar el archivo PDF y el nombre y carpeta donde guardarlo.")
+        if not texto_var.get().strip() and not os.path.isabs(entrada_imagen_var.get()): 
+            raise ValueError("Debes introducir un texto o una imagen como marca de agua.")
+        if not color_var.get() and not os.path.isabs(entrada_imagen_var.get()):
             raise ValueError("Debes elegir un color para el texto.")
-        
-        # Parámetros para la función
+    
+        if os.path.isfile(entrada_imagen_var.get()) and not color_var.get():
+            color_var.set((0,0,0))
+
+        #Parámetros para la función
         anadir_marca_agua_a_pdf(
             ruta_entrada_pdf=entrada_var.get(),
             ruta_salida_pdf=salida_var.get(),
@@ -104,33 +158,44 @@ def ejecutar():
             opacidad_texto=opacidad_var.get(),
             texto_mayusculas=mayusculas_var.get(),
             color_texto=eval(color_var.get()),  # Convierte el string en tupla
-            blanco_y_negro=bn_var.get()
+            blanco_y_negro=bn_var.get(),
+            ruta_imagen=entrada_imagen_var.get()
         )
+
         messagebox.showinfo("Éxito", f"PDF procesado correctamente.\nEl archivo se ha guardado en:\n{salida_var.get()}")
+        nombre_archivo_var.set("")
+        salida_var.set("Aún no se ha elegido un lugar de guardado")
     except Exception as e:
         messagebox.showerror("Error", f"Ocurrió un error: {str(e)}\nPara más información contacte con el desarrollador.")
 
 def reiniciar():
     """Restablece todos los valores de entrada."""
     entrada_var.set("Aún no se ha elegido un archivo PDF")
+    entrada_imagen_var.set("Aún no se ha elegido una imagen")
     salida_var.set("Aún no se ha elegido un lugar de guardado")
     texto_var.set("")
+    entrada_texto.config(state="normal")
     tamano_var.set(16)
     opacidad_var.set(100)
     mayusculas_var.set(False)
     color_var.set("")
+    entrada_color.config(state="normal")
     bn_var.set(False)
     nombre_archivo_var.set("")
     # Restablecer el label del color
-    color_label.config(bg=root.cget("bg"), text="")
+    marco_color.config(state="normal")
+    marco_color.config(bg=root.cget("bg"), text="")
+    entrada_mayus.config(state="normal")
+    entrada_minus.config(state="normal")
     #borrar el contenido del marco
     if hasattr(marco_imagen, "imagen_label"):
         marco_imagen.imagen_label.destroy()
         del marco_imagen.imagen_label
 
+
 # Crear ventana principal
 root = tk.Tk()
-root.title("Añadir Marca de Agua a PDF")
+root.title("Añadir Marca de Agua a PDF. versión 1.0")
 
 # Tamaño fijo para cada frame
 ANCHO_FRAME = 450
@@ -187,6 +252,7 @@ canvas_der.create_window((0, 0), window=frame_interior_der, anchor="nw")
 
 #---------------------VARIABLES----------------------------------------
 entrada_var = tk.StringVar(value="Aún no se ha elegido un archivo PDF")
+entrada_imagen_var =  tk.StringVar(value="Aún no se ha elegido una imagen")
 salida_var = tk.StringVar(value="Aún no se ha elegido un lugar de guardado")
 texto_var = tk.StringVar()
 tamano_var = tk.IntVar(value=16)
@@ -229,33 +295,53 @@ marco_cuadrado_texto = tk.Frame(frame_interior_izq, padx=10, pady=10, relief="so
 marco_cuadrado_texto.pack(padx=10, pady=4, fill="x")
 
 tk.Label(marco_cuadrado_texto, text="Escriba el texto de la marca de agua (máx. 30 caracteres):", font=("Arial", 9, "bold underline")).pack(anchor="w")
-tk.Entry(marco_cuadrado_texto, textvariable=texto_var, width=35).pack(anchor="w")
+entrada_texto = tk.Entry(marco_cuadrado_texto, textvariable=texto_var, width=35)
+entrada_texto.pack(anchor="w")
+
+tk.Label(marco_cuadrado_texto, text="O bien, escoja una imagen para usarla como marca de agua:", font=("Arial", 9, "bold underline")).pack(anchor="w")
+tk.Label(marco_cuadrado_texto, text="(se bloquearán ciertas opciones)", font=("Arial", 7)).pack(anchor="w")
+
+frame_botones_imagenes = tk.Frame(marco_cuadrado_texto)
+tk.Button(frame_botones_imagenes, text="Elegir imagen", command=seleccionar_imagen).pack(side="left", padx=(0, 10))
+tk.Button(frame_botones_imagenes, text="Eliminar imagen", command=quitar_imagen).pack(side="left")
+frame_botones_imagenes.pack(anchor="w")
+
+frame_ruta_imagen = tk.Frame(marco_cuadrado_texto)
+tk.Label(frame_ruta_imagen, text="Ruta imagen:", fg="black").pack(side="left")
+tk.Label(frame_ruta_imagen, textvariable=entrada_imagen_var, fg="blue").pack(side="left")
+frame_ruta_imagen.pack(anchor="w")
 
 #-----------------------------------------------------------------------------------------------
 
 marco_cuadrado_color = tk.Frame(frame_interior_izq, padx=10, pady=10, relief="solid", bd=1)
 marco_cuadrado_color.pack(padx=10, pady=4, fill="x")
 
+# Etiqueta como estaba originalmente
 tk.Label(marco_cuadrado_color, text="Color del texto de la marca de agua:", font=("Arial", 9, "bold underline")).pack(anchor="w")
+
+# Frame para los elementos de color
 color_frame = tk.Frame(marco_cuadrado_color)
-tk.Button(color_frame, text="Elegir Color", command=seleccionar_color).pack(side="left", padx=(0, 10))
-color_label = tk.Label(color_frame, width=2, height=1, relief="flat")
-color_label.pack(side="left")
-color_frame.pack(anchor="w")
+color_frame.pack(anchor="w")  # Mantengo el anchor="w" como en tu código original
+
+# Botón a la izquierda y marco de color a la derecha dentro de color_frame
+entrada_color = tk.Button(color_frame, text="Elegir Color", command=seleccionar_color)
+entrada_color.pack(side="left", padx=(0, 10))  # Padding para separar del marco_color
+marco_color = tk.Label(color_frame, width=2, height=1, relief="flat")
+marco_color.pack(side="right")
 
 #-----------------------------------------------------------------------------------------------
 
 marco_cuadrado_tamano = tk.Frame(frame_interior_izq, padx=10, pady=10, relief="solid", bd=1)
 marco_cuadrado_tamano.pack(padx=10, pady=4, fill="x")
 
-tk.Label(marco_cuadrado_tamano, text="Seleccione el tamaño del texto (16-40):", font=("Arial", 9, "bold underline")).pack(anchor="w")
+tk.Label(marco_cuadrado_tamano, text="Seleccione el tamaño de la marca (16-40):", font=("Arial", 9, "bold underline")).pack(anchor="w")
 tk.Scale(marco_cuadrado_tamano, from_=16, to=40, variable=tamano_var, orient="horizontal").pack(anchor="w")
 #-----------------------------------------------------------------------------------------------
 
 marco_cuadrado_opacidad = tk.Frame(frame_interior_izq, padx=10, pady=10, relief="solid", bd=1)
 marco_cuadrado_opacidad.pack(padx=10, pady=4, fill="x")
 
-tk.Label(marco_cuadrado_opacidad, text="Transparencia del texto (0-255):", font=("Arial", 9, "bold underline")).pack(anchor="w")
+tk.Label(marco_cuadrado_opacidad, text="Seleccione la transparencia de la marca (0-255):", font=("Arial", 9, "bold underline")).pack(anchor="w")
 tk.Scale(marco_cuadrado_opacidad, from_=0, to=255, variable=opacidad_var, orient="horizontal").pack(anchor="w")
 #-----------------------------------------------------------------------------------------------
 
@@ -263,8 +349,10 @@ marco_cuadrado_mayus = tk.Frame(frame_interior_izq, padx=10, pady=10, relief="so
 marco_cuadrado_mayus.pack(padx=10, pady=4, fill="x")
 
 frame_mayus = tk.Frame(marco_cuadrado_mayus)
-tk.Radiobutton(frame_mayus, text="Minúsculas", variable=mayusculas_var, value=False).pack(side="left")
-tk.Radiobutton(frame_mayus, text="Mayúsculas", variable=mayusculas_var, value=True).pack(side="left")
+entrada_mayus = tk.Radiobutton(frame_mayus, text="Minúsculas", variable=mayusculas_var, value=False)
+entrada_mayus.pack(side="left")
+entrada_minus = tk.Radiobutton(frame_mayus, text="Mayúsculas", variable=mayusculas_var, value=True)
+entrada_minus.pack(side="left")
 frame_mayus.pack(anchor="w")
 
 #-----------------------------------------------------------------------------------------------
@@ -283,6 +371,8 @@ button_frame.pack(side="left")
 button_frame.pack(pady=10)
 #-----------------------------------------------------------------------------------------------
 #------------------------WIDGETS FRAME DERECHO---------------------------------------------------
+
+tk.Label(frame_interior_der, text="Visualizador de vista previa", font=("Arial", 9, "underline")).pack(anchor="w")
 
 # Crear marco sin bordes para la imagen (más alta que ancha)
 marco_imagen = tk.Frame(frame_interior_der, width=400, height=500, relief="flat", bd=0, bg="white")
@@ -314,6 +404,16 @@ def _on_mousewheel(event):
             canvas_izq.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
 root.bind_all("<MouseWheel>", _on_mousewheel)
+
+
+def cerrar_programa():
+    """Función que se ejecuta al cerrar la ventana para limpiar y salir."""
+    print("Cerrando programa...")  #Mensaje opcional
+    root.quit()  #Detiene el loop de Tkinter
+    root.destroy()  #Cierra la ventana completamente
+    exit()  #Asegura que todo el proceso termine
+
+root.protocol("WM_DELETE_WINDOW", cerrar_programa)
 
 if __name__ == "__main__":
     root.mainloop()
--- marcar_agua_pdf.py
+++ marcar_agua_pdf.py
@@ -1,7 +1,8 @@
 """
-Modulo que contiene funciones para añadir una marca de agua en un pdf. En desarrollo.
+Modulo que contiene funciones para añadir una marca de agua en un pdf.
 """
 
+import os
 import fitz
 from PIL import Image, ImageDraw, ImageFont
 import io
@@ -13,7 +14,7 @@ __license__ = "GPL"
 __version__ = "1.0"
 __maintainer__ = ""
 __email__ = "https://github.com/moraloamg"
-__status__ = "Development"
+__status__ = "Completed"
 
 
 def _crear_marca_agua(texto:str,
@@ -81,6 +82,42 @@ def _crear_marca_agua(texto:str,
     return recuadro
 
 
+def _crear_marca_agua_imagen(ruta_imagen: str = None, tamano: int = 20, opacidad: int = 100):
+    """
+    Función que crea una marca de agua a partir de una imagen, permitiendo ajustar su tamaño y opacidad.
+
+    Parameters:
+    ----------
+    - ruta_imagen (str): Ruta de la imagen que se usará como marca de agua.
+    - tamano (int): Tamaño de la imagen en porcentaje respecto al original (recomendado entre 16 y 100).
+    - opacidad (int): Opacidad de la imagen (recomendado entre 0 y 255, donde 255 es completamente opaco).
+
+    Returns:
+    --------
+    - imagen (PIL.Image): Imagen de la marca de agua ajustada.
+    """
+
+    #Cargamos la imagen aseguramos el canal alfa
+    imagen = Image.open(ruta_imagen).convert("RGBA")
+    
+    #Ajustamos el tamaño de la imagen en función del porcentaje especificado
+    ancho_original, alto_original = imagen.size
+    nuevo_ancho = int(ancho_original * (tamano / 100))
+    nuevo_alto = int(alto_original * (tamano / 100))
+    imagen = imagen.resize((nuevo_ancho, nuevo_alto), Image.Resampling.LANCZOS)
+
+    #Aplicamos la opacidad modificando el canal alfa
+    r, g, b, a = imagen.split()
+    a = a.point(lambda p: p * (opacidad / 100))  # Ajustar la opacidad en base al porcentaje
+    imagen = Image.merge("RGBA", (r, g, b, a))
+
+    #Rotamos la imagen 45° y expandimos para evitar recortes
+    imagen = imagen.rotate(45, expand=True)
+
+    return imagen
+
+
+
 def vista_previa_pdf(
         ruta_entrada_pdf: str,
         texto_marca_agua: str,
@@ -88,32 +125,64 @@ def vista_previa_pdf(
         opacidad_texto: int,
         texto_mayusculas: bool = False,
         color_texto: tuple[int, int, int] = (255, 0, 0),
-        blanco_y_negro: bool = False):
+        blanco_y_negro: bool = False,
+        ruta_imagen: str = None):
     
+    """
+    Función que genera una vista previa del pdf. Hace una captura de la primera página y añade la marca
+    de agua, para luego ser enviada a la interfaz gráfica.
+    Si hay una ruta de una imagen en su respectiva variable, se devolverá una marca de agua con dicha imagen,
+    en caso contrario, se devolverá una marca de agua con un texto.
+
+    Parameters:
+    ----------
+    - ruta_entrada_pdf (str): Ruta del pdf a marcar.
+    - texto_marca_agua (str): Texto saldrá en la marca de agua.
+    - ruta_imagen (str): Ruta de la imagen de la marca de agua.
+    - tamano_fuente (int): Tamaño de la fuente (recomentado ente 16 y 40).
+    - opacidad_texto (int): Opacidad del texto (recomendado en 100).
+    - texto_mayusculas (bool): Poner el texto en minúsculas con False o mayúsculas con True. False por defecto.
+    - color_texto (tuple[int,int,int]): tupla que indica el color en tres dígitos. Rojo por defecto.
+    - blanco_y_negro (bool): True convierte el pdf a blanco y negro, False lo deja como está. False por defecto.
+    - ruta_imagen (str): Ruta de la imagen. Si hay una ruta, se pondrá la marca de agua con la imagen y no con el texto.
+
+    Returns:
+    --------
+    - buffer_imagen (BytesIO): Imagen guardada en memoria.
+    """
+    
+    #Seleccionamos la primera página del pdf
     documento = fitz.open(ruta_entrada_pdf)
     pagina = documento[0]
 
-    # Convertir la primera página a imagen
+    #Convertimos la primera página a imagen a una imagen
     pix = pagina.get_pixmap()
     imagen = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
 
+    #Convertimos la imagen a blanco y negro si está la correspondiente opción activada
     if blanco_y_negro:
-        # Convertir solo la imagen del PDF a escala de grises antes de aplicar la marca de agua
-        imagen = imagen.convert("L").convert("RGB")  # "L" convierte a gris, luego "RGB" para mantener el formato
+        #Convertimos solo la imagen del PDF a escala de grises antes de aplicar la marca de agua
+        imagen = imagen.convert("L").convert("RGB")  #"L" convierte a gris, luego "RGB" para mantener el formato
 
-    # Crear la marca de agua en color
-    marca_agua = _crear_marca_agua(texto_marca_agua, tamano_fuente, opacidad_texto, texto_mayusculas, color_texto)
+    #Si hay una marca de agua, la añadimos, si no, añadimos texto
+    if not os.path.isfile(ruta_imagen): 
+        #Creamos la marca de agua de texto en color
+        marca_agua = _crear_marca_agua(texto_marca_agua, tamano_fuente, opacidad_texto, texto_mayusculas, color_texto)
+    else:
+        #Creamos la marca de agua con imagen si hay imagen
+        marca_agua = _crear_marca_agua_imagen(ruta_imagen, tamano_fuente, opacidad_texto)
 
-    # Aplicar la marca de agua en la imagen
+    #Aplicamos la marca de agua en la imagen
     for x in range(0, imagen.width, marca_agua.width + 10):
         for y in range(0, imagen.height, marca_agua.height + 1):
             imagen.paste(marca_agua, (x, y), marca_agua)
 
-    # Guardar la imagen final en un buffer
+    #Guardamos la imagen final en un buffer
     buffer_imagen = io.BytesIO()
     imagen.save(buffer_imagen, format="JPEG", quality=50)
     buffer_imagen.seek(0)
 
+    #cerramos el documento y retornamos la imagen guardada en memoria
     documento.close()
     return buffer_imagen
 
@@ -127,12 +196,15 @@ def anadir_marca_agua_a_pdf(
         texto_mayusculas:bool = False,
         color_texto: tuple[int, int, int] = (255, 0, 0),
         blanco_y_negro: bool = False,
+        ruta_imagen:str = None,
         espaciado_ancho:int = 10,
         espaciado_alto:int = 1):
     
     """
     Función que repite una marca de agua en mosaico a cada página de un PDF.
-    La cantidad de teselas del mosaico puede verse afectada por la separación horizontal o vertical
+    La cantidad de teselas del mosaico puede verse afectada por la separación horizontal o vertical.
+    Si hay una ruta de una imagen en su respectiva variable, se devolverá una marca de agua con dicha imagen,
+    en caso contrario, se devolverá una marca de agua con un texto.
 
     Parameters:
     ----------
@@ -144,6 +216,7 @@ def anadir_marca_agua_a_pdf(
     - texto_mayusculas (bool): Poner el texto en minúsculas con False o mayúsculas con True. False por defecto.
     - color_texto (tuple[int,int,int]): tupla que indica el color en tres dígitos. Rojo por defecto.
     - blanco_y_negro (bool): True convierte el pdf a blanco y negro, False lo deja como está. False por defecto.
+    - ruta_imagen (str): Ruta de la imagen. Si hay una ruta, se pondrá la marca de agua con la imagen y no con el texto.
     - espaciado_ancho (int): Separación horizontal del texto dispuesto en la marca de agua.
     - espaciado_alto (int): Separación vertical del texto dispuesto en la marca de agua.
 
@@ -155,7 +228,10 @@ def anadir_marca_agua_a_pdf(
     #Abre el PDF de entrada con PyMuPDF
     documento = fitz.open(ruta_entrada_pdf)
     #Crea la imagen de la marca de agua
-    imagen_marca_agua = _crear_marca_agua(texto_marca_agua, tamano_fuente, opacidad_texto, texto_mayusculas, color_texto)
+    if not os.path.isfile(ruta_imagen):
+        imagen_marca_agua = _crear_marca_agua(texto_marca_agua, tamano_fuente, opacidad_texto, texto_mayusculas, color_texto)
+    else:
+        imagen_marca_agua = _crear_marca_agua_imagen(ruta_imagen,tamano_fuente, opacidad_texto)
     #Crea un buffer en memoria para almacenar la imagen
     bytes_imagen = io.BytesIO()
     #Guarda la imagen en el buffer como PNG
@@ -200,8 +276,5 @@ def anadir_marca_agua_a_pdf(
 
 #------------------------------- FIN DEL CODIGO ---------------------------------------
 
-
-#TODO comentar funcion vista previa
-#TODO poner funcion o funciones para hacer lo mismo con una imagen
 
 
